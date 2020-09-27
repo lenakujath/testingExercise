f1c2cf160f41f7816e15d92a6f72bf87e23a75ae
Lena Kujath <l.kujath@gmx.de>
2020-09-27 16:20:26 +0200

    added own solution to symmetric difference problem

--- js/request-logic.js
+++ js/request-logic.js
@@ -11,3 +11,48 @@ function getContentType(filename) {
 };
 
 console.log(getContentType('lena.jpg'));
+
+//freecodeCamp Algorithms
+
+// 1.Symmetric Difference
+
+function checkForSameVal(first, second) {
+
+  let arrFinal = [];
+
+    for (let i = 0; i < first.length; i++){
+        if (second.includes(first[i]) == false){
+            arrFinal.push(first[i])       
+        }  
+    };
+    for (let j = 0; j < second.length; j++){
+        if (first.includes(second[j]) == false){
+            arrFinal.push(second[j])       
+        }  
+    }; 
+    return getUniqueValues(arrFinal);
+}
+
+const getUniqueValues = (arr) => {
+  let unique = [...new Set(arr)]
+  return unique;
+}
+
+function sym(...args) {   
+
+     let count = 0;
+     let resultArr = [];
+   
+     for (let i = 0; i < args.length - 1; i++){
+      resultArr = checkForSameVal(args[0], args[1 + count]);
+      args[0] = resultArr;      
+      count ++;
+     };
+
+     
+     return resultArr;
+  }
+
+ // console.log(sym([2, 3, 3, 5, 5, 8], [2, 3,  6, 7], [3, 3, 5, 7]));
+
+ 
