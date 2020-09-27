ec8df3feedd70ef4c7de823393433f9d6c284e4f
Lena Kujath <l.kujath@gmx.de>
2020-09-27 16:23:46 +0200

    added solution to reduce complexity

--- js/request-logic.js
+++ js/request-logic.js
@@ -67,4 +67,20 @@ const sym = (...args) => [...new Set(args.reduce(diff))];
 // test here
 sym([1, 2, 3], [5, 2, 1, 4]);
 
-//
+//explanation: https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-find-the-symmetric-difference/301611/2
+
+function sym(...args){
+  return [...args.reduce(reducer, new Set())]
+}
+
+function reducer(result, arr){
+  const compare = new Set(arr);
+  for(let val of compare){
+    if(result.has(val)){
+      result.delete(val);
+    }else{
+      result.add(val)
+    }
+  }
+  return result;
+}
