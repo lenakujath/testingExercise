6f1161d6c70c416056cb4a7f337c7e90efa2da73
Lena Kujath <l.kujath@gmx.de>
2020-10-01 13:46:58 +0200

    added second solution to string reverse

--- js/string-reverse.js
+++ js/string-reverse.js
@@ -1,6 +1,13 @@
 const reverse = (str) => {
-
     return str.split('').reverse().join('');
 };
 
-console.log(reverse('hello'))
+const reverseSecond = (str) => {
+    let reverse = "";
+    for (let i = str.length -1; i >= 0; i--) {
+        reverse += str[i];        
+    }
+    return reverse;
+}
+
+console.log(reverseSecond('hello'))
